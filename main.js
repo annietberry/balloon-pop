8541c7472804e337e7e3da9857c8688aea2ab806
annietberry <tombariannie@gmail.com>
2021-03-22 14:15:59 -0700

    Balloon pops at max size and button moved to top

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxSize = 300
+
+
 function inflate(){
+  clickCount++
   var balloonElement = document.getElementById("balloon")
-  balloonElement.style.height = "400px"
-  balloonElement.style.width = "300px"
+  height += inflationRate
+  width += inflationRate
+  if(height >= maxSize){
+    console.log("pop the baloon")
+    height = 0
+    width = 0
+  }
+  balloonElement.style.height = height + "px"
+  balloonElement.style.width = width + "px"
+
+
+  var clickCountElem = document.getElementById("click-count")
+  clickCountElem.innerText = clickCount.toString()
 }
