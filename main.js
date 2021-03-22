6348915a0e07be0310667548396313af6173f665
annietberry <tombariannie@gmail.com>
2021-03-22 14:44:04 -0700

    Added Balloon Pop Count for User

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ let height = 120
 let width = 100
 let inflationRate = 20
 let maxSize = 300
-
+let popCount = 0
 
 function inflate(){
   clickCount++
-  var balloonElement = document.getElementById("balloon")
+  let balloonElement = document.getElementById("balloon")
   height += inflationRate
   width += inflationRate
+  
   if(height >= maxSize){
-    console.log("pop the baloon")
+    console.log("pop the balloon")
+    popCount++
     height = 0
     width = 0
+    document.getElementById('pop-count').innerText = popCount.toString()
   }
+
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
 
 
-  var clickCountElem = document.getElementById("click-count")
+  let clickCountElem = document.getElementById("click-count")
   clickCountElem.innerText = clickCount.toString()
 }
