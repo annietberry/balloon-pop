e21483b02c5fae846cad97766951c2bd7c1886af
annietberry <tombariannie@gmail.com>
2021-03-22 17:02:18 -0700

    Abstracting draw

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let startButton = document.getElementById('start-button')
+let inflateButton = document.getElementById('inflate-button')
+
 let clickCount = 0
 let height = 120
 let width = 100
@@ -5,15 +8,29 @@ let inflationRate = 20
 let maxSize = 300
 let popCount = 0
 
+
 function startGame(){
+
+  startButton.disabled = true
+  inflateButton.removeAttribute('disabled')
+
   setTimeout(() => {
     console.log("it's been three seconds")
+
+    inflateButton.disabled = true
+    startButton.removeAttribute('disabled')
+
+    clickCount = 0
+    height = 120
+    width = 100
+
+    draw()
+
   }, 3000)
 }
 
 function inflate(){
   clickCount++
-  let balloonElement = document.getElementById("balloon")
   height += inflationRate
   width += inflationRate
   
@@ -21,13 +38,18 @@ function inflate(){
     popCount++
     height = 0
     width = 0
-    document.getElementById('pop-count').innerText = popCount.toString()
   }
+  draw()
+}
 
+function draw(){
+  let balloonElement = document.getElementById("balloon")
+  let clickCountElem = document.getElementById("click-count")
+  let popCountElem = document.getElementById('pop-count')
+  
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
-
-
-  let clickCountElem = document.getElementById("click-count")
+  
   clickCountElem.innerText = clickCount.toString()
+  popCountElem.innerText = popCount.toString()
 }
